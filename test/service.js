07a205b9041b0ba084214579c3a9b4e20a539efa
Bo Sun <bsun@axioma.com>
2015-09-03 11:11:24 -0400

    start random service based on args "include-random"

--- test/service.js
+++ test/service.js
@@ -2,11 +2,20 @@
  * Created by bsun on 8/28/2015.
  */
 
+var yargs = require('yargs');
+
 var seneca = require('seneca')();
 
-// seneca.use(require('./random'), {});
+var argv = require('yargs').argv;
 
-var proxy = require('./index')(seneca.client({port: 9292, host: 'localhost'}));
+var proxy = null;
+// whether start random service in this node process
+if (argv['include-random']) {
+  seneca.use(require('./random'), {});
+  proxy = require('./index')(seneca);
+} else {
+  proxy = require('./index')(seneca.client({port: 9292, host: 'localhost'}));
+}
 
 seneca.add({generate: 'id'}, function (msg, response) {
   proxy.generateId(function (err, result) {
